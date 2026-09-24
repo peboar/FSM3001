69b8e747e15674e7f1239beb60c83603b79a7171
per <perboekhout@hotmail.com>
2026-09-24 11:02:48 +0200

    ninth version 20260924

--- src/main_ct.py
+++ src/main_ct.py
@@ -4,13 +4,24 @@ import sys
 import config_data as cfg
 from ct.ct_generator import CtDataGenerator
 
-project_path = script_path.parent
+project_path = Path(__file__).resolve().parent
 data_path = project_path / "data" / (cfg.AGGREGATE_TYPE + "s")
 
 if __name__ == "__main__":
-    for path in data_path.iterdir()[0]:
+    for path in data_path.iterdir():
+        packing_paths  = list(path.glob("*.npz"))
+
+        if len(packing_paths) < 1:
+            raise Exception("Number of packings must be greater than 1")
+
+        if len(packing_paths) > 1:
+            print("Warning the number of packings exceeds 1")
+            print("Fallback to the first one")
+
+        packing_path = packing_paths[0]
+
         ct_generator = CtDataGenerator(
-            packing_path=path,
+            packing_path=packing_path,
             image_size=cfg.IMAGE_SIZE,
             image_extension=cfg.IMAGE_EXTENSION,
             dpi=cfg.DPI,
